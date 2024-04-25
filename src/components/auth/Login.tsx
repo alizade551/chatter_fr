2c836da0772aa832135ea0d8b1a61090f6de0a6b
alizade551 <merkuri.93@mail.ru>
2024-04-25 17:41:01 +0400

    Login

--- src/components/auth/Login.tsx
+++ src/components/auth/Login.tsx
@@ -1,9 +1,11 @@
 import { Link } from 'react-router-dom';
 import Auth from './Auth';
+import { useLogin } from '../../hooks/useLogin';
 
 const Login = () => {
+  const { login, error } = useLogin();
   return (
-    <Auth submitLabel='Login' onSubmit={async () => {}}>
+    <Auth submitLabel='Login' onSubmit={async (request) => login(request)}>
       <Link
         id='RouterNavLink'
         to='/signup'
